c51e78b5214df7e7666e24dc059de30d3e6cc2bf
ELyas <97180899+elyas-ely@users.noreply.github.com>
2025-05-09 16:33:31 +0430

    good

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,16 +1,30 @@
-import { pgTable, serial, varchar, timestamp } from 'drizzle-orm/pg-core'
+import {
+  pgTable,
+  serial,
+  varchar,
+  timestamp,
+  boolean,
+} from 'drizzle-orm/pg-core'
 import { z } from 'zod'
 
 export const users = pgTable('users', {
   user_id: serial('user_id').primaryKey(),
   username: varchar('username', { length: 255 }).notNull(),
   email: varchar('email', { length: 255 }).notNull().unique(),
+  bio: varchar('bio', { length: 255 }),
+  seller: boolean('seller').notNull().default(false),
+  profile: varchar('profile'),
+  background: varchar('background'),
   createdAt: timestamp('created_at').defaultNow(),
 })
 
 export const createUserSchema = z.object({
-  name: z.string().min(2),
-  email: z.string().email(),
+  username: z.string().min(1).max(255),
+  email: z.string().email().max(255),
+  bio: z.string().max(255).optional(),
+  seller: z.boolean().default(false),
+  profile: z.string().optional(),
+  background: z.string().optional(),
 })
 
 export type CreateUserInput = z.infer<typeof createUserSchema>
